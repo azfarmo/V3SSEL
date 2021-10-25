341a7f8ba7276a84f76a6ab1642cedcce5b822a3
Azfar <azfar@Azfars-MacBook-Pro.local>
2021-10-25 12:26:52 -0400

    Updates

---
+++ precache-manifest.07c620b004d0971db72e9608fca673fb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8593e092c9f4c5552a54006df050a14c",
+    "url": "/V3SSEL/index.html"
+  },
+  {
+    "revision": "3223d3143420efbf6d3e",
+    "url": "/V3SSEL/static/css/2.516d9e09.chunk.css"
+  },
+  {
+    "revision": "33405ad9e148865d2476",
+    "url": "/V3SSEL/static/css/main.f154ae49.chunk.css"
+  },
+  {
+    "revision": "3223d3143420efbf6d3e",
+    "url": "/V3SSEL/static/js/2.1be96d06.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/V3SSEL/static/js/2.1be96d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab125520e534f17298c0",
+    "url": "/V3SSEL/static/js/3.abfc6fef.chunk.js"
+  },
+  {
+    "revision": "33405ad9e148865d2476",
+    "url": "/V3SSEL/static/js/main.02824eb0.chunk.js"
+  },
+  {
+    "revision": "6f97f8c8218a1d76c425",
+    "url": "/V3SSEL/static/js/runtime-main.da370ba9.js"
+  },
+  {
+    "revision": "7719f4e3a4acc3b7b664242482fcb50c",
+    "url": "/V3SSEL/static/media/bg-home-banner.7719f4e3.jpg"
+  },
+  {
+    "revision": "89e5a37ab700984f3f8a629ddd623b36",
+    "url": "/V3SSEL/static/media/chart.89e5a37a.png"
+  },
+  {
+    "revision": "566b749ace2e6b2904054af5f666d1f8",
+    "url": "/V3SSEL/static/media/home-image2.566b749a.svg"
+  },
+  {
+    "revision": "224c5e891567c13c78ced09fcf1478d8",
+    "url": "/V3SSEL/static/media/home-image3.224c5e89.svg"
+  },
+  {
+    "revision": "cd56502778de8d1401e9456731ac7247",
+    "url": "/V3SSEL/static/media/home-image4.cd565027.svg"
+  },
+  {
+    "revision": "f73cf5bbd5e52f5c90c29e20cd573615",
+    "url": "/V3SSEL/static/media/logo.f73cf5bb.png"
+  },
+  {
+    "revision": "24009cab3a13fb3ca5743da7b5ba141d",
+    "url": "/V3SSEL/static/media/modal.24009cab.png"
+  },
+  {
+    "revision": "cd5024438c1c93dd358c5f746e01960f",
+    "url": "/V3SSEL/static/media/partner.cd502443.png"
+  },
+  {
+    "revision": "bd2dda2268e5a93a3922519f5b1afbee",
+    "url": "/V3SSEL/static/media/team-member.bd2dda22.png"
+  },
+  {
+    "revision": "95724e7bf9670837825cf565ce4ea350",
+    "url": "/V3SSEL/static/media/team-member2.95724e7b.png"
+  },
+  {
+    "revision": "58f6164da634b3fc251eb9eec5993e9e",
+    "url": "/V3SSEL/static/media/team.58f6164d.png"
+  },
+  {
+    "revision": "edcd467eb74dc98d798450a5c95a32b3",
+    "url": "/V3SSEL/static/media/team2.edcd467e.png"
+  },
+  {
+    "revision": "624d0240fc766c1db5b50ba06c38da1b",
+    "url": "/V3SSEL/static/media/wave.624d0240.svg"
+  },
+  {
+    "revision": "a86e9871b49384ce0d7919f0796f8dc7",
+    "url": "/V3SSEL/static/media/who.a86e9871.png"
+  }
+]);
